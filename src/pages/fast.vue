9340945247bbc44615651d5d8c4a56b529e0c2f6
IanGuen <jos.nguyen1120@gmail.com>
2025-09-24 14:14:23 +0700

    feat(1.25.4): change Icon of clear result button

--- src/pages/fast.vue
+++ src/pages/fast.vue
@@ -59,12 +59,8 @@ async function handleOnTypeAnalyze() {
             viewBox="0 0 24 24"
           >
             <path
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M20 11A8.1 8.1 0 0 0 4.5 9M4 5v4h4m-4 4a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"
+              fill="currentColor"
+              d="M7 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2h4a1 1 0 1 1 0 2h-1.069l-.867 12.142A2 2 0 0 1 17.069 22H6.93a2 2 0 0 1-1.995-1.858L4.07 8H3a1 1 0 0 1 0-2h4zm2 2h6V4H9zM6.074 8l.857 12H17.07l.857-12zM10 10a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1m4 0a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1"
             />
           </svg>
         </NButton>
