e0cb145ecb75ac5c05683d302962577aae4018ec
IanGuen <jos.nguyen1120@gmail.com>
2025-10-09 14:15:41 +0700

    feat!: improve database with json assets

--- src/pages/fun.vue
+++ src/pages/fun.vue
@@ -23,6 +23,7 @@ import {
   parseWordList,
   parseWordData,
 } from "@/lib/words";
+import dictionary from "@/assets/dictionary.json";
 
 const loadingItem = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"> <circle cx="18" cy="12" r="0" fill="currentColor"> <animate attributeName="r" begin=".67" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0" /> </circle> <circle cx="12" cy="12" r="0" fill="currentColor"> <animate attributeName="r" begin=".33" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0" /> </circle> <circle cx="6" cy="12" r="0" fill="currentColor"> <animate attributeName="r" begin="0" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0" /> </circle> </svg>`;
 const listRef = useTemplateRef<HTMLElement>("listRef");
@@ -121,10 +122,14 @@ async function handleRequest() {
 
   let content = "";
   let res: any;
+  const lemmaList = Object.keys(dictionary);
 
   const command = wordList.shift();
   switch (command) {
-    case "/sentence":
+    case "/dict":
+      pushRequest(
+        `Test dict ${lemmaList.filter((v) => v.includes(wordList[0])).length}`,
+      );
       break;
 
     case "/words":
